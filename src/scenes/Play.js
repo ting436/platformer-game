23cfd1a3af69c183d2effd400b4285eb22db07d6
ting436 <154811950+ting436@users.noreply.github.com>
2024-11-14 03:20:23 +0000

    adding preupdate to player class

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -10,12 +10,9 @@ class Play extends Phaser.Scene {
     create() {
         const map = this.createMap();
         const layers = this.createLayers(map);
+        const player = this.createPlayer();
 
-        this.player = this.createPlayer();
-
-        this.playerSpeed = 200;
-        this.physics.add.collider(this.player, layers.platformColliders);
-        this.cursors = this.input.keyboard.createCursorKeys();
+        this.physics.add.collider(player, layers.platformColliders);
     }
 
     createMap() {
@@ -36,22 +33,7 @@ class Play extends Phaser.Scene {
     }
 
     createPlayer() {
-        const player = new Player(this, 100, 250);
-        player.body.setGravityY(500);
-        player.setCollideWorldBounds(true);
-        return player;
-    }
-
-    update() {
-        const { left, right } = this.cursors;
-
-        if (left.isDown) {
-            this.player.setVelocityX(-this.playerSpeed);
-        } else if (right.isDown) {
-            this.player.setVelocityX(this.playerSpeed);
-        } else {
-            this.player.setVelocityX(0);
-        }
+        return new Player(this, 100, 250);
     }
 }
 export default Play;
